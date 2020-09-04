4ce5df06700b8f1341bcabd2361f09b98e648740
nguyentrongdoan <nguyendoan206@gmail.com>
2020-09-04 15:31:26 +0700

    them IUserRepository

--- Test/Data/DataContext.cs
+++ Test/Data/DataContext.cs
@@ -8,5 +8,6 @@ namespace Test.Data
         public DataContext(DbContextOptions<DataContext> options) : base(options){}
         
         public DbSet<Customer> Customers { get; set; }
+        public DbSet<User> Users { get; set; }
     }
 }
