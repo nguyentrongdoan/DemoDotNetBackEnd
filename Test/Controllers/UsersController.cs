41c0b1083a8b4253a7c1f598eab27d2d06b58888
nguyentrongdoan <nguyendoan206@gmail.com>
2020-09-09 17:15:17 +0700

    Finishing off the Member edit component

--- Test/Controllers/UsersController.cs
+++ Test/Controllers/UsersController.cs
@@ -1,4 +1,6 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Claims;
 using System.Threading.Tasks;
 using AutoMapper;
 using Microsoft.AspNetCore.Authorization;
@@ -36,6 +38,20 @@ namespace Test.Controllers
             var user = await _repository.GetUser(id);
             var userToReturn = _mapper.Map<UserForDetailedDto>(user);
             return Ok(userToReturn);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateUser(int id, UserForUpdateDto userForUpdateDto)
+        {
+            if (id != int.Parse(User.FindFirst(ClaimTypes.NameIdentifier).Value))
+                return Unauthorized();
+            var userFromRepo = await _repository.GetUser(id);
+            _mapper.Map(userForUpdateDto, userFromRepo);
+
+            if (await _repository.SaveAll())
+                return NoContent();
+           
+            throw new Exception($"Updating user {id} failed on save");
         }
     }
 }
