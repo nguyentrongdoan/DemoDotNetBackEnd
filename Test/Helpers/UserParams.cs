dea969e48e590ae4b91772a0bfe94e9f44db1d0a
nguyentrongdoan <nguyendoan206@gmail.com>
2020-09-14 13:50:00 +0700

    Sorting results in the API

--- Test/Helpers/UserParams.cs
+++ Test/Helpers/UserParams.cs
@@ -17,5 +17,11 @@ namespace Test.Helpers
         public int UserId { get; set; }
         
         public string Gender { get; set; }
+
+        public int MinAge { get; set; } = 18;
+        
+        public int MaxAge { get; set; } = 99;
+        
+        public string OrderBy { get; set; }
     }
 }
